93c4e2247dfdebd6bb9877739140e9083f729368
MTR <github@hilscher.com>
2019-09-05 13:10:55 +0200

    netx4000: Implement reset

--- arch/arm/mach-netx4000/reset.c
+++ arch/arm/mach-netx4000/reset.c
@@ -1,5 +1,15 @@
+#include <common.h>
+#include <linux/io.h>
+
+#define RAP_SYSCTRL_RSTCTRL 0xf8000050
+#define RAP_SYSCTRL_RSTMASK 0xf8000054
+
 void reset_cpu(unsigned long ignored)
 {
-	//TODO:
+	uint32_t reset_mask = 0;
+
+	reset_mask = readl(RAP_SYSCTRL_RSTMASK);
+	writel((reset_mask | 1), RAP_SYSCTRL_RSTMASK);
+	writel(1, RAP_SYSCTRL_RSTCTRL);
 	while (1) ;
 }
