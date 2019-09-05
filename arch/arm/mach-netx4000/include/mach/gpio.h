377b69056c4b4430f5990ac07f4ac2a266f1c433
MTR <github@hilscher.com>
2019-09-05 14:51:34 +0200

    netx4000: Add gpio.h

---
+++ arch/arm/mach-netx4000/include/mach/gpio.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright 2019 Hilscher Gesellschaft fuer Systemautomation mbH
+ */
+
+#ifndef __ASM_ARCH_NETX4000_GPIO_H
+#define __ASM_ARCH_NETX4000_GPIO_H
+
+#endif /* __ASM_ARCH_NETX4000_GPIO_H */
+
