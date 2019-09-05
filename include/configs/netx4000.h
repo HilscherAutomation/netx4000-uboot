43ea012f0e26330286bb92701a765075ddd34816
MTR <github@hilscher.com>
2019-09-05 09:39:27 +0200

    boot: Use boot.scr on first fat partition on MMC for controlling boot

--- include/configs/netx4000.h
+++ include/configs/netx4000.h
@@ -10,7 +10,7 @@
 #include <linux/sizes.h>
 
 #define CONFIG_SYS_SDRAM_BASE	0x40000000
-#define CONFIG_SYS_LOAD_ADDR	0x05000000
+#define CONFIG_SYS_LOAD_ADDR	0x42000000
 #define CONFIG_SYS_INIT_SP_ADDR	0x0507F000
 #define CONFIG_SYS_MALLOC_LEN	(8 * SZ_1M)
 
@@ -19,9 +19,24 @@
 #define CONFIG_EXTRA_ENV_SETTINGS \
 	"fdtaddr=0x44000000\0"    \
 	"fdt_high=0x44010000\0"   \
+	"mmcdev=0\0" \
+	"mmcpart=1\0" \
+	"script=boot.scr\0" \
+	"loadbootscript=" \
+		"fatload mmc ${mmcdev}:${mmcpart} ${loadaddr} ${script};\0" \
+	"bootscript=echo Running bootscript from mmc ...; " \
+		"source\0" \
 	"bootargs_legacy=root=/dev/mmcblk0p2 rw console=ttyAMA0,115200 earlyprintk\0" \
 	"bootcmd_legacy=fatload mmc 0:1 ${loadaddr} fitImage; setenv bootargs ${bootargs_legacy}; bootm\0"
 
-#define CONFIG_BOOTCOMMAND	"run bootcmd_legacy"
+#define CONFIG_BOOTCOMMAND	"mmc dev ${mmcdev}; if mmc rescan; then " \
+					"if run loadbootscript; then " \
+						"run bootscript; " \
+					"else " \
+						"run bootcmd_legacy; " \
+					"fi; " \
+				"else " \
+					"run bootcmd_legacy;" \
+				"fi"
 
 #endif /* __CONFIG_H */
