c857a4e9c55f91f49c52452eab9fa807bdbb54bc
Michael Trensch <mtrensch@hilscher.com>
2021-02-24 11:44:51 +0100

    netx4000,reset: When u-boot was booted as alternate firmware only alternate firmwares were booted after reset.
    This is caused by the ROM loader remembering alternate boot state in it's internal memory.

--- arch/arm/mach-netx4000/reset.c
+++ arch/arm/mach-netx4000/reset.c
@@ -8,6 +8,10 @@ void reset_cpu(unsigned long ignored)
 {
 	uint32_t reset_mask = 0;
 
+	/* Clear RAM that the ROM code uses to remember booting alternate firmware */
+	memset((void*)0x05100000, 0, 256);
+	dmb();
+
 	reset_mask = readl(RAP_SYSCTRL_RSTMASK);
 	writel((reset_mask | 1), RAP_SYSCTRL_RSTMASK);
 	writel(1, RAP_SYSCTRL_RSTCTRL);
